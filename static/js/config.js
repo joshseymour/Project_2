1320faadad0f13829e87503366bb70494281555d
Josh <josh.seymour@insight.com>
2019-10-07 12:41:42 -0400

    gitignore

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,4 @@
-const API_KEY = "pk.eyJ1Ijoiam9zZXltb3UiLCJhIjoiY2swZWZnYm5iMGhoMTNtcDg1aHJrMnBwaiJ9.jzYOLsmXfLICnKA_16fS4w";
-const apiKey = "AIzaSyBp0_QDE1LQPcT6RaNTDQidDh_XVSMnDxE"
+const API_KEY = {
+    MY_KEY: "pk.eyJ1Ijoiam9zZXltb3UiLCJhIjoiY2swZWZnYm5iMGhoMTNtcDg1aHJrMnBwaiJ9.jzYOLsmXfLICnKA_16fS4w",
+    apiKey: "AIzaSyBp0_QDE1LQPcT6RaNTDQidDh_XVSMnDxE"
+}
--- static/js/logic.js
+++ static/js/logic.js
@@ -164,7 +164,7 @@ var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
 attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 maxZoom: 18,
 id: "mapbox.streets",
-accessToken: API_KEY
+accessToken: config.API_KEY
 });
 
 //create your base map
